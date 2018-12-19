dc7319b7e56f5bfa8b92efd9484cd8cfebb195f7
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-19 10:37:10 -0500

    creando el singleton

--- platziverse-db/lib/db.js
+++ platziverse-db/lib/db.js
@@ -1,1 +1,12 @@
+'use strict'
+
+const Sequelize =require('sequelize')
+let sequelize=null
+
+module.exports=function setupDatabase(config){
+  if(!sequilize){
+    sequelize=new Sequelize(config)
+  }
+  return sequelize
+}
 
