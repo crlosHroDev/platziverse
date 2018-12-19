ac99f20b1ece903e21a9c7db873c99dd44344101
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-19 11:41:55 -0500

    Implementación de un módulo básico BD

--- platziverse-db/index.js
+++ platziverse-db/index.js
@@ -1,6 +1,23 @@
 'use strict'
 
+const setupDatabase=require('./lib/db')
+const setupAgentModel=require('./models/agent')
+const setupMetricModel=require('./models/metric')
+
+
 module.exports=async function (config){
+  
+  const sequelize=setupDatabase(config)//Aqui ya llamo el singleton
+  const AgentModel=setupAgentModel(config)
+  const MetricModel=setupMetricModel(config)
+  
+  //Definir las relaciones Sequelize crea automatamente las FK
+  AgentModel.hasMany(MetricModel)
+  MetricModel.belongsTo(AgentModel)
+  
+  await sequelize.authenticate() //validar que la BD este bien validada
+ 
+  
   const Agent={}
   const Metric={}
   
