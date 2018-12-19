5553d346b622dcbe804295bf35ee497939826de3
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-19 17:30:46 -0500

    Creación de fixtures
     definición básica del servicio de Agent

--- platziverse-db/tests/agent-tests.js
+++ platziverse-db/tests/agent-tests.js
@@ -4,6 +4,8 @@ const test=require('ava')
 const sinon=require('sinon')
 const proxyquire=require('proxyquire')
 
+const agentFixtures=require('./fixtures/agent')
+
 let config={
   logging:function(){}
 }
@@ -12,6 +14,8 @@ let MetricStub={
   belongsTo:sinon.spy()//spy es una funcion especifica que me va a permitir hacerle preguntas
 }
 
+let Single=Object.assign({},agentFixtures.single)//Cuando estemos probando los stubs podamos probar con una instancia por aparte
+let id=1
 let AgentStub=null
 let db=null
 let sandbox=null//sanbox cuando se vuelva a llamar el test se reinician los valores para reiniciar uno diferente
@@ -41,4 +45,10 @@ test.serial('Setup',t=>{//Garantizar que las funciones fueron llamadas por Sinon
   t.true(AgentStub.hasMany.calledWith(MetricStub),'Argument should be the Model')//calledWith es para decir que fue llamada especificamente con un argumento 
   t.true(MetricStub.belongsTo.called,'MetricModel.belongsTo was executed')
   t.true(MetricStub.belongsTo.calledWith(AgentStub),'Argument should be the Model')
+})
+
+test.serial('Agent#findById',async t =>{
+  let agent=await db.Agent.findById(id)
+  
+  t.deepEqual(agent,agentFixtures.byId(id),'should be the same')
 })
