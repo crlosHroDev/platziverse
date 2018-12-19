3361265def4104e04c0e1e2eeada1d849caca1de
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-19 15:48:21 -0500

    Introducción a pruebas unitarias con Ava.js

--- platziverse-db/tests/agent-tests.js
+++ platziverse-db/tests/agent-tests.js
@@ -1,1 +1,17 @@
+'use strict'
 
+const test=require('ava')
+
+let config={
+  logging:function(){}
+}
+let db=null
+
+test.before(async()=>{
+  const setupDatabase=require('../')
+  db=await setupDatabase(config)
+})
+
+test('Agent',t=>{
+  t.truthy(db.Agent,'Agent service should exist')//Que ese valor resuelva verdadero, es decir que no sea vacio
+})
