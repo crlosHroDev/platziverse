de57c1a28fb91319fb7bff7c8fe6368c5322562d
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-20 12:51:58 -0500

    Implementación del servidor MQTT

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -1,1 +1,24 @@
+'use strict'
 
+const debud=require('debug')('platziverse:mqtt')
+const mosca=require('mosca')
+const redis=require('redis')
+const chalk=require('chalk')
+
+const backend={
+  type:'redis',
+  redis,
+  return_buffers:true //viene binaria y es mas facil para redis
+}
+
+const settings={
+  port:1883,//puerto por default
+  backend
+}
+
+//instanciar el servidor
+const server=new mosca.Server(settings)
+
+server.on('ready',()=>{
+  console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
+})
