d026b988da1232c3d3b9f09b04812ba66d152bfd
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-20 14:14:34 -0500

    Integrar el servidor MQTT con BD

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -4,6 +4,7 @@ const debug=require('debug')('platziverse:mqtt')
 const mosca=require('mosca')
 const redis=require('redis')
 const chalk=require('chalk')
+const db=require('platziverse-db')
 
 const backend={
   type:'redis',
@@ -16,8 +17,19 @@ const settings={
   backend
 }
 
+ const config={
+    database:process.env.DB_NAME||'platziverse',
+    username:process.env.DB_USER||'platzi',
+    password:process.env.DB_PASS||'platzi',
+    host:process.env.DB_HOST||'localhost',
+    dialect:'postgres',
+    logging:s=>debug(s)
+  }
+
 //instanciar el servidor
 const server=new mosca.Server(settings)
+
+let Agent,Metric
 
 server.on('clientConnected',client=>{
   debug(`Client Connected: ${client.id}`) //este id es autogenerado por mqtt
@@ -32,7 +44,11 @@ server.on('published',(packet,client)=>{
   debug(`Payload: ${packet.payload}`) //payload es el objeto, la info enviada
 })
 
-server.on('ready',()=>{
+server.on('ready',async ()=>{
+  const services=await db(config).catch(handleFatalError)//se instancia la bd aqui porque en esta parte se inicia el server
+  
+  Agent=services.Agent
+  Metric=services.Metric
   console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
 })
 
