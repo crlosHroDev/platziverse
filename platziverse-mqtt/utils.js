c5a6d689542b7ee13bbfe7b95c011537426886db
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-20 15:01:31 -0500

    Almacenar informacion del agente
    Cómo almacenar la información del agente y reportar el agente conectado

--- platziverse-mqtt/utils.js
+++ platziverse-mqtt/utils.js
@@ -1,1 +1,18 @@
+'use strict'
 
+function parsePayload(payload){
+  if(payload instanceof Buffer){
+    payload=payload.toString('utf8')
+  }
+  
+  try{
+    payload=JSON.parse(payload)
+  }catch(e){
+    payload={}
+  }
+  return payload
+}
+
+module.exports={
+  parsePayload
+}
