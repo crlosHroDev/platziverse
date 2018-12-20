185dd5e19757b7d94d48a73ddea65b22dd611afa
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-20 15:01:28 -0500

    Almacenar informacion del agente
    Cómo almacenar la información del agente y reportar el agente conectado

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -6,6 +6,9 @@ const redis=require('redis')
 const chalk=require('chalk')
 const db=require('platziverse-db')
 
+const {parsePayload}=require('./utils')
+
+
 const backend={
   type:'redis',
   redis,
@@ -28,19 +31,60 @@ const settings={
 
 //instanciar el servidor
 const server=new mosca.Server(settings)
+const clients=new Map()
 
 let Agent,Metric
 
 server.on('clientConnected',client=>{
   debug(`Client Connected: ${client.id}`) //este id es autogenerado por mqtt
+  clients.set(client.id,null)
 })
 
 server.on('clientDisconnected',client=>{
   debug(`Client Disonnected: ${client.id}`) 
 })
 
-server.on('published',(packet,client)=>{
+server.on('published',async (packet,client)=>{
   debug(`Received: ${packet.topic}`)//topic es el tipo de mensaje
+  
+  switch(packet.topic){
+    case 'agent/connected':
+    case 'agent/connected':
+      debug(`Payload:${packet.payload}`)
+      break
+    case 'agent/message':
+      const payload=parsePayload(packet.payload)
+      if(payload){
+        payload.agent.connected=true
+        
+        let agent
+        try{
+          agent=await Agent.createOrUpdate(payload.agent)
+        }catch(e){
+          return handleError(e)
+        }
+        debug(`Agent ${agent.uuid} saved`)
+        
+        //Notify Agent is Connected
+        
+        if(!clients.get(client.id)){
+          clients.set(client.id,agent)
+          server.publish({
+            topic:'agent/connected',
+            payload:JSON.stringify({
+              agent:{
+                uuid:agent.uuid,
+                name:agent.name,
+                hostname:agent.hostname,
+                pid:agent.pid,
+                connected:agent.connected
+              }
+            })
+          })
+        }
+      }
+      break
+  }
   debug(`Payload: ${packet.payload}`) //payload es el objeto, la info enviada
 })
 
@@ -58,6 +102,11 @@ function handleFatalError(err){
   console.error(`${chalk.red('fatal error')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
+}
+
+function handleError(err){
+   console.error(`${chalk.red('fatal error')} ${err.message}`)
+   console.error(err.stack)
 }
 
 process.on('uncaughtException',handleFatalError)
