0f6ab3603d1b2c757eb8ffb8e5fee35aa0a27e0d
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-21 15:28:20 -0500

    Definición de rutas del API

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -1,1 +1,28 @@
+'use strict'
 
+const debug=require('debug')('platziverse:api:router')
+const express=require('express')
+
+const api=express.Router()
+
+api.get('/agents',(req,res)=>{
+  debug('A request has come to /agents')
+  res.send({})
+})
+
+
+api.get('/agents/:uuid',(req,res)=>{ //luego de los dos puntos uuid llegaria al objeto como un parametro
+  const {uuid}=req.params
+  res.send({uuid})
+}) 
+api.get('/metrics/:uuid',(req,res)=>{
+  const {uuid}=req.params
+  res.send({uuid})
+})
+
+api.get('/metrics/:uuid/:type',(req,res)=>{
+  const {uuid,type}=req.params
+  res.send({uuid,type})
+})
+
+module.exports=api
