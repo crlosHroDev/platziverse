2ca5dece872e189f2556ebadaced32bc526261f5
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-21 17:04:39 -0500

    Pruebas basicas de integracion con Ava y Supertest

--- platziverse-api/tests/api-tests.js
+++ platziverse-api/tests/api-tests.js
@@ -1,1 +1,19 @@
+'use strict'
 
+const test=require('ava')
+const request=require('supertest')
+
+const server=require('../server')
+
+test.serial.cb('/api/agents',t=>{//estamos probando funciones de tipo callback, ya que supertest trabaja con callbacks
+  request(server)
+    .get('/api/agents')
+    .expect(200) //espero que me retorne 
+    .expect('Content-Type',/json/)
+    .end((err,res)=>{
+      t.falsy(err,'should not return an error')
+      let body=res.body
+      t.deepEqual(body,{},'response body should be the expected')
+      t.end()//Este t end solo lo necesito cuando estoy usando el tipo cb
+  })
+})
