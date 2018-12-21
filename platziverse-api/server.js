49c51502f99463536cf0fc7a20816a9ffdaaf520
crlosHroDev <45835073+crlosHroDev@users.noreply.github.com>
2018-12-21 15:28:23 -0500

    Definición de rutas del API

--- platziverse-api/server.js
+++ platziverse-api/server.js
@@ -4,10 +4,13 @@ const http=require('http')
 const chalk=require('chalk')
 const express=require('express')//Se va a encargar de crear una request handler que se ejecutará cada vez que llega una peticion a nuestro servidor
 
+const api=require('./api')
+
 const port=process.env.PORT|| 3000
 const app=express()
 const server=http.createServer(app)
 
+api.use('/api',api)//Funcion para montar middlewares en express. Montar en la ruta /api que sera /api/agent
 server.listen(port,()=>{
   console.log(`${chalk.green('[platziverse-api]')} server listening on port ${port}`)
 })
